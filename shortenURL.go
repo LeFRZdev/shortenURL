c01a5ebece4af0a5cb402b5f7b5718ffae6ec51f
LeFRZdev <frizzyop@gmail.com>
2021-01-20 09:19:40 +0100

    start

--- shortenURL.go
+++ shortenURL.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"fmt"
+	"github.com/kare/base62"
+)
+
+func main() {
+	var urlVal int64 = 2222222
+	encodeURL := base62.Encode(urlVal)
+	fmt.Println(encodeURL)
+
+	byteURL, _ := base62.Decode(encodeURL)
+	fmt.Println(byteURL)
+}
