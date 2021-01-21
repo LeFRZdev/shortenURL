b799e2e04a2ae4eccbbceccd59a896169d2dff08
LeFRZdev <frizzyop@gmail.com>
2021-01-21 12:41:16 +0100

    decode

--- go.mod
+++ go.mod
@@ -4,5 +4,7 @@ go 1.15
 
 require (
 	github.com/gorilla/mux v1.8.0
+	github.com/pkg/browser v0.0.0-20210115035449-ce105d075bb4
+	golang.org/x/sys v0.0.0-20210119212857-b64e53b001e4 // indirect
 	kkn.fi/base62 v0.1.0
 )
--- shortenURL.go
+++ shortenURL.go
@@ -8,15 +8,17 @@ import (
 	"time"
 	"log"
 	"sync"
+	"github.com/pkg/browser"
 )
 
 var mu sync.Mutex
 var nb int64 = 1
+var addr string = "http://localhost:8000/"
+var URLs map[string]int64
 
 func URLHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	w.WriteHeader(http.StatusOK)
-	URLs := make(map[string]int64)
 	URLs[vars["URL"]] = nb
 	encodeURL := base62.Encode(nb)
 	fmt.Fprintf(w, "URL : %v\n", encodeURL)
@@ -26,9 +28,20 @@ func URLHandler(w http.ResponseWriter, r *http.Request) {
 	mu.Unlock()
 }
 
+func URL2(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	URLs[vars["URL"]] = nb
+	encodeURL := base62.Decode(nb)
+	fmt.Fprintf(w, "URL : %v\n", encodeURL)
+	fmt.Fprintf(w, "nb : %v\n", nb)
+}
+
 func main() {
+	URLs = make(map[string]int64)
 	r := mux.NewRouter()
-	r.HandleFunc("/{URL}", URLHandler)
+	r.HandleFunc("/{URL}", URL2)
+	r.HandleFunc("/new/{URL}", URLHandler)
 	srv := &http.Server{
 		Handler: r,
 		Addr: "127.0.0.1:8000",
